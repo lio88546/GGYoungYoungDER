0768c4744788d9b701b41b8d0e309a626516c3aa
lio88546 <strikewichesh@gmail.com>
2018-09-07 16:19:53 +0800

    complete register & login not game linked

--- logReg/src/components/logReg.vue
+++ logReg/src/components/logReg.vue
@@ -1,79 +1,93 @@
 <template>
-  <div id="con">
-    <div id="regs" v-if="member.act=='regs'">
-      <h2>註冊會員</h2>
-      <p>帳號/暱稱：</p>
-      <el-input style="width:20em" v-model="registerData.name" placeholder="請輸入帳號/暱稱"></el-input>
-      <br>
-      <p>密碼：</p><el-input type="password" style="width:20em" v-model="registerData.psw" placeholder="請輸入密碼"></el-input>
-      <br>
-      <p>本名：</p><el-input type="text" style="width:20em" v-model="registerData.realName" placeholder="請輸入您的本名"></el-input>
-      <br>
-      <p>性別：</p>
-      <el-radio v-model="registerData.sex" label="male">男</el-radio>
-      <el-radio v-model="registerData.sex" label="female">女</el-radio>
-      <el-radio v-model="registerData.sex" label="other">中性</el-radio>
-      <br>
-      <p>年齡：</p>
-      <el-select v-model="registerData.age" placeholder="請選擇年齡區間">
-        <el-option
-          v-for="item in ageOptions"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <br>
-      <p>地址：</p><el-input type="text" style="width:20em" v-model="registerData.adddress" placeholder="請輸入地址"></el-input>
-      <br>
-      <p>電話：</p><el-input type="text" style="width:20em" v-model.number="registerData.phone" placeholder="請輸入您的本名"></el-input>
-      <br>
-      <p>國家：</p>
-      <el-select v-model="registerData.country" placeholder="請選擇國家">
-        <el-option
-          v-for="item in countryOptions"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <br><br>
-      <button @click="member.act='login'">返回</button> <button @click="regs()">註冊</button>
-    </div>
-    <div id="login" else v-if="member.act=='login'">
-      <h2>會員登入</h2>
-      <p>帳號/暱稱：</p>
-      <el-input style="width:20em" v-model="loginData.name" placeholder="請輸入帳號/暱稱"></el-input>
-      <br>
-      <p>密碼：</p><el-input type="password" style="width:20em" v-model="loginData.psw" placeholder="請輸入密碼"></el-input>
-      <br><br>
-      <button @click="member.act='regs'">註冊</button> <button @click="login()">登入</button>
-    </div>
-  </div>
+  <el-row :gutter="0">
+    <el-col :span="6" :offset="9">
+      <div v-show="member.hint!=''">
+        <p style="font-size: 2.5em ; font-weight: bold">{{member.hint}}</p>
+      </div>
+      <div id="regs"  v-if="member.act=='regs'">
+        <h2>註冊會員</h2>
+        <p>帳號/暱稱：</p>
+        <el-input style="width:20em" v-model="registerData.name" placeholder="請輸入帳號/暱稱"></el-input>
+        <br>
+        <p>密碼：</p><el-input type="password" style="width:20em" v-model="registerData.psw" placeholder="請輸入密碼"></el-input>
+        <br>
+        <p>本名：</p><el-input type="text" style="width:20em" v-model="registerData.realName" placeholder="請輸入您的本名"></el-input>
+        <br>
+        <p>性別：</p>
+        <el-radio v-model="registerData.sex" label="male">男</el-radio>
+        <el-radio v-model="registerData.sex" label="female">女</el-radio>
+        <el-radio v-model="registerData.sex" label="other">中性</el-radio>
+        <br>
+        <p>年齡：</p>
+        <el-select v-model="registerData.age" placeholder="請選擇年齡區間">
+          <el-option
+            v-for="item in ageOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <br>
+        <p>國家：</p>
+        <el-select v-model="registerData.country" placeholder="請選擇國家">
+          <el-option
+            v-for="item in countryOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <br>
+        <p>地址：</p><el-input type="text" style="width:20em" v-model="registerData.address" placeholder="請輸入地址"></el-input>
+        <br>
+        <p>電話：</p><el-input type="text" style="width:20em" v-model.number="registerData.phone" placeholder="請輸入您的本名"></el-input>
+        <br><br>
+        <button @click="member.act='login'">返回</button> <button @click="regs()">註冊</button>
+      </div>
+      <div id="login" else v-if="member.act=='login'">
+        <h2>會員登入</h2>
+        <p>帳號/暱稱：</p>
+        <el-input style="width:20em" v-model="loginData.name" placeholder="請輸入帳號/暱稱"></el-input>
+        <br>
+        <p>密碼：</p><el-input type="password" style="width:20em" v-model="loginData.psw" placeholder="請輸入密碼"></el-input>
+        <br><br>
+        <button @click="member.act='regs'">註冊</button> <button @click="login()">登入</button>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+
+var socket = io.connect("http://192.168.30.117:8542");//與伺服器建立連結
+//向伺服器發送的訊息，確認連接狀況良好
+socket.emit('condition',{condition:'Client Connection!'});
+//接收伺服器發送的訊息，確認連接狀況良好
+socket.on('message', function(msg){
+  console.log(msg.connection);
+});
+
 export default {
   name: 'logReg',
   data () {
     return {
-      loginData:{
+      loginData:{//登入資料
         task:"",
         name:"",
         psw:""
       },
-      registerData:{
+      registerData:{//註冊資料
         task:"",
         name:"",
         psw:"",
         realName:"",
         sex:"",
         age:"",
-        adddress:"",
-        phone:"",
-        country:""
+        country:"",
+        address:"",
+        phone:""
       },
-      ageOptions: [{
+      ageOptions: [{//年齡選項
         value: '<=19',
         label: '19歲(含)以下'
       }, {
@@ -92,7 +106,7 @@ export default {
         value: '>=40',
         label: '40歲(含)以上'
       }],
-      countryOptions:[{
+      countryOptions:[{//國家選項
         value: 'taiwan',
         label: '台灣'
       }, {
@@ -117,47 +131,89 @@ export default {
         hint:"",
       }
     }
+  }, 
+  mounted() {
+    //接註冊回應
+    socket.on('regsResult',(result) => {
+      this.member.act = "";
+      if(result.response == 'success'){
+        this.member.hint = this.registerData.name+"註冊成功，現在將導向到登入頁面";
+      } else {
+        this.member.hint = "註冊失敗";
+      }
+      setTimeout( () => {
+        this.member.hint = "";
+        this.member.act = "login";
+      },3000);
+    });
+
+    //接登入回應
+    socket.on('logResult',(result) => {
+      if(result.response == 'success'){
+        this.member.act = "";
+        this.member.hint = this.loginData.name+"登入成功，現在將導向到遊戲頁面";
+        setTimeout( () => {
+          this.member.hint = "";
+          this.member.act = "login";
+          //this.member.is_member = true;
+        },3000);
+      } else {
+        this.member.act = "";
+        this.member.hint = "帳號/密碼錯誤";
+        setTimeout( () => {
+          this.member.hint = "";
+          this.member.act = "login";
+        },3000);
+      }
+    });
   },
   methods:{
     regs(){
-      console.log(this.registerData);
-      if(false){
-
-        
-
+      this.registerData.task = "regs";
+      if(this.check(this.registerData)){
+        console.log('access');
         //至server處理
-        //this.inputData.task = "regs";
-        //socket.emit('regs',this.memberData);
+        socket.emit('regs',this.registerData);
       } else {
         alert("ERROR");
       }
     },
 
     login(){
-      console.log(this.loginData);
-      if(false){
+      this.loginData.task = "login";
+      if(this.check(this.loginData)){
+        //至server處理
         
-        //this.inputData.task = "login";
-        //socket.emit('login',this.inputData);
+        socket.emit('login',this.loginData);
       } else {
         alert("請輸入暱稱與密碼以進行登入");
       }
+    },
+    check(Obj){
+      let con = true;
+      for(name of Object.keys(Obj)){
+        if(Obj[name]=="" && name!="address" && name!="phone"){
+          console.log(name);
+          con = false;
+          break;
+        }
+      }
+      return con;
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-  #regs, #login{
-    width: 50%;
-    margin:auto;
-    
+  h2{
+    text-align: center;
   }
 
-  #login {
-    line-height: 3;
+  #login,#regs {
+    line-height: 4;
   }
 
 
--- logRegServer/logRegServer.js
+++ logRegServer/logRegServer.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response){
     console.log('Connection');//印出連接字樣
   });
   
-server.listen(8080);//伺服器port點指定
+server.listen(8542);//伺服器port點指定
   
 var serv_io = io.listen(server);// 開啟 Socket.IO 的 listener
 
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
   host: "192.168.30.118",
   user: "root",
   password: "passwords",
-  database: "git"
+  database: "git",
 });
 
 //連接資料庫
@@ -38,44 +38,78 @@ serv_io.sockets.on('connection', function(socket) {
   //var playerData = {};//玩家資料
 
   //註冊
-  socket.on('regs',function(menberData) {
-    console.log("task : "+menberData.task);
-    /*
-    sql = "INSERT INTO "+
-          "pokergame ( name, psw)"+
-          " VALUES ( '" + menberData.name + "', '"+
-                         menberData.psw + "' ) ;"
-    connection.query(sql, function (err, result) {
+  socket.on('regs',function(regData) {
+    console.log("task : "+regData.task);
+
+    let today = new Date();
+    //查詢是否重複
+    checkSql = "SELECT * FROM member " + 
+               "WHERE id='" + regData.name + "' || name='" + regData.realName + "'";
+    connection.query(checkSql, function(err, result){
       if (err) throw err;
-      socket.emit('regsResult',{name : menberData.name});
+      if(result.length > 0) {
+        socket.emit('regsResult',{response : 'faile'});
+        return;
+      }     
+      //註冊SQL
+      registerSql = "INSERT INTO "+
+            "member ( id, passwords, name, sex, age, country, address, phone, regdate, regtime, record, money)"+
+            " VALUES ( '" + regData.name + "', '"+
+                            regData.psw + "', '"+
+                            regData.realName + "', '"+
+                            regData.sex + "', '"+
+                            regData.age + "', '"+
+                            regData.country + "', '"+
+                            regData.address + "', '"+
+                            regData.phone + "', '"+
+                            today.toLocaleDateString() + "', '"+
+                            today.toLocaleTimeString() + "', '"+
+                            "Register" + "', '"+
+                            10000 + "' ) ;";
+      connection.query(registerSql, function (err, result) {
+        if (err) throw err;
+        socket.emit('regsResult',{response : 'success'});
+        //寫入LOG檔SQL
+        logfileSql = "INSERT INTO "+
+        "log ( date, time, ip, type, note)"+
+        " VALUES ( '" + today.toLocaleDateString() + "', '"+
+                        today.toLocaleTimeString() + "', '"+
+                        socket.request.connection.remoteAddress + "', '"+
+                        "Register" + "', '"+
+                        regData.name + " Register " + "' ) ;";
+        connection.query(logfileSql, function (err, result) {
+          if (err) throw err;
+        });
+      });
+      
     });
-    */
   });
 
   //登入
-  socket.on('login',function(menberData) {
-    console.log("task : "+menberData.task);
-    /*
-    sql = "SELECT * FROM pokergame " + 
-          "WHERE name='" + menberData.name + "' AND" + 
-          " psw='" + menberData.psw + "'";
+  socket.on('login',function(loginData) {
+    console.log("task : "+loginData.task);
+    
+    sql = "SELECT * FROM member " + 
+          "WHERE id='" + loginData.name + "' && passwords='" + loginData.psw + "'";
     connection.query(sql,function(error, result, fields){
       if(result != ""){
         let today = new Date();//取當下Date
-        sql = "UPDATE pokergame SET "+
-              "loginTime = '" + today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate() + 
-              " " + today.getHours()+":"+today.getMinutes()+":"+today.getSeconds() + "'" +
-              " WHERE " + 
-              "id = " + result[0].id + ";";
-        connection.query(sql,function(error) {
-          if (error) throw error;
-          playerData = result[0];//存放玩家於資料庫之ID
-          socket.emit('logResult',result[0]);
+        //寫入LOG檔SQL
+        logfileSql = "INSERT INTO "+
+        "log ( date, time, ip, type, note)"+
+        " VALUES ( '" + today.toLocaleDateString() + "', '"+
+                        today.toLocaleTimeString() + "', '"+
+                        socket.request.connection.remoteAddress + "', '"+
+                        "Login" + "', '"+
+                        loginData.name + " Register " + "' ) ;";
+        connection.query(logfileSql, function (err, result) {
+          if (err) throw err;
+          socket.emit('logResult',{response : 'success'});
         });
       } else {
-        socket.emit('logResult',{id:-1});
+        socket.emit('logResult',{response : 'faile'});
       }
     });
-    */
+    
   });
 });
